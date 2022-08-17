5255b2964a4773947f1a3f0c6852969b98229874
r4v-su <63302890+r4v-su@users.noreply.github.com>
2022-08-17 22:16:53 +0200

    change styling

--- cube.js
+++ cube.js
@@ -1,12 +1,9 @@
-"use strict";
+const debugFlag = true,
 
-const CONTAINER = document.createElement('div');
-
-CONTAINER.setAttribute('id', 'CONTAINER');
+CONTAINER = document.createElement('div');
+CONTAINER.setAttribute('id', 'CONTAINER'),
 document.body.appendChild(CONTAINER);
-
-const debug = true,
-    log = function (arg) {if (debug) {console.log(arg)}},
+const
 
     CANVAS_STYLING = {
         bgcol: 'black',
@@ -15,8 +12,8 @@ const debug = true,
     },
 
     CANVAS_MEASUREMENTS = {
-        width: 600, //set ANY
-        height: 600, //set ANY
+        width: 300, //set ANY
+        height: 300, //set ANY
         scale: 1,
         dotSize: 5,
         startAngle: 0,
@@ -28,11 +25,6 @@ const debug = true,
         y: CANVAS_MEASUREMENTS.height * 0.5
     };
 
-log(CANVAS_POINT_ZERO);
-
-log(`CANVAS_MEASUREMENTS : ${Object.entries(CANVAS_MEASUREMENTS)}`);
-log(`CANVAS_POINT_ZERO :  ${Object.entries(CANVAS_POINT_ZERO)}`);
-
 const CTX = document.createElement('canvas').getContext('2d');
 CTX.canvas.width = CANVAS_MEASUREMENTS.width;
 CTX.canvas.height = CANVAS_MEASUREMENTS.height;
@@ -42,23 +34,15 @@ CTX.canvas.setAttribute('id', 'cube');
 const PLAY_BUTTON = document.createElement('button');
 PLAY_BUTTON.innerHTML = 'play';
 PLAY_BUTTON.setAttribute('id', 'PLAY_BUTTON');
-PLAY_BUTTON.style.position = 'absolute';
-PLAY_BUTTON.style.left = `${CANVAS_POINT_ZERO.x + CANVAS_MEASUREMENTS.dotSize}px`;
-PLAY_BUTTON.style.transform = 'translateX(-50%)';
-PLAY_BUTTON.style.top = `${(2 * CANVAS_POINT_ZERO.y)}px`;
-CONTAINER.appendChild(PLAY_BUTTON);
 
 const PROGRESS_BAR = document.createElement('progress');
 PROGRESS_BAR.setAttribute('type', 'range');
 PROGRESS_BAR.setAttribute('max', '200');
 PROGRESS_BAR.setAttribute('value', '0');
 PROGRESS_BAR.setAttribute('id', 'throwBar');
-PROGRESS_BAR.style.position = 'absolute';
-PROGRESS_BAR.style.width = '50px';
 PROGRESS_BAR.style.top = `${(1.7*CANVAS_POINT_ZERO.y)}px`;
-PROGRESS_BAR.style.left = `${CANVAS_POINT_ZERO.x + CANVAS_MEASUREMENTS.dotSize}px`;
-PROGRESS_BAR.style.transform = `translateX(-50%)`;
 CONTAINER.appendChild(PROGRESS_BAR);
+CONTAINER.appendChild(PLAY_BUTTON);
 
 const CUBE_WALLS = {
     1: [
@@ -96,10 +80,12 @@ const CUBE_WALLS = {
         [CANVAS_POINT_ZERO.x - 2 * CANVAS_MEASUREMENTS.dotSize, CANVAS_POINT_ZERO.y, CANVAS_MEASUREMENTS.dotSize, CANVAS_MEASUREMENTS.startAngle, CANVAS_MEASUREMENTS.endAngle, true] // centerright
     ]
 };
+
 const drawEdge = function () {
     CTX.fillStyle = CANVAS_STYLING.edgecol;
     CTX.strokeRect(CANVAS_POINT_ZERO.x - (CANVAS_MEASUREMENTS.dotSize * 5), CANVAS_POINT_ZERO.y - (CANVAS_MEASUREMENTS.dotSize * 5), CANVAS_MEASUREMENTS.dotSize * 10, CANVAS_MEASUREMENTS.dotSize * 10)
 };
+
 const drawWall = function (wall) {
     CUBE_WALLS[wall].forEach(dot => {
         CTX.beginPath();
@@ -131,7 +117,8 @@ const throwCube = function () {
     if (frameID == 200) {
         PLAY_BUTTON.disabled = false;
         document.getElementById('throwBar').value = frameID;
-        log(result);
+        myLog(result, debugFlag);
+
     } else
     if (frameID < 200) {
         drawEdge();
@@ -140,7 +127,7 @@ const throwCube = function () {
             result = (pickRandomNumber(max));
             clearWall();
             drawWall(result);
-        }
+        } return result;
     }
 };
 const newGame = function () {
@@ -149,4 +136,5 @@ const newGame = function () {
     clearWall();
     throwCube();
 }
+
 PLAY_BUTTON.addEventListener('click', newGame);
